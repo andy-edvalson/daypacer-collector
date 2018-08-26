bafebba959c3dcc09182d45ea0988effd45e4e83
Andy Edvalson <a_edvalson@gmail.com>
2018-08-25 21:21:47 -0600

    Removing express server, converting to lambda

---
+++ lambda_handler.js
@@ -0,0 +1,52 @@
+var sdb = require('./sdb')
+
+exports.handler = function(event, context) {
+  var self = this;
+
+  self.init = function(event, context) {
+    self.event = event;
+    self.context = context;
+    self.db = new sdb();
+    self.validateData(event)
+  };
+
+  self.validateData = function(obj) {
+    let phoneNumber = obj['phone']
+    // TODO: Require fields
+    // TODO: Whitelist fields
+    // TODO: Validate phone numbers
+
+    self.db.phoneNumberExists(phoneNumber).then((exists) => {
+      if (exists) { return self.sendRejectResponse("Phone number already exists") }
+      self.addRow(obj)
+    }).catch((err) => {
+      self.sendErrorResponse("Error Adding Row", err)
+    })
+  };
+
+  self.addRow = function(obj) {
+    this.db.addRow(obj).then(function(rowId) {
+      self.sendAcceptResponse(rowId)
+    }).catch((err) => {
+      self.sendErrorResponse("Error Adding Row", err)
+    })
+  };
+
+  self.sendAcceptResponse = function(rowId) {
+    self.event.response = {success: true, accepted: true, id: rowId}
+    self.context.done(null, self.event);
+  };
+
+  self.sendRejectResponse = function(msg) {
+    self.event.response = {success: true, accepted: false, message: msg}
+    context.done(null, self.event);
+  };
+
+  self.sendErrorResponse = function(msg, err) {
+    console.log(err)
+    self.event.response = {success: false, message: err}
+    context.done(null, self.event);
+  };
+
+  this.init(event, context);
+}
--- start.js
+++
@@ -1,3 +0,0 @@
-var sdb_server = require('./sdb_server')
-
-var server = new sdb_server().start()
